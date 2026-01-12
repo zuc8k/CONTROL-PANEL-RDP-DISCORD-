5d88146899c61c9c63ed9b4ad9a4ea73ccf1b4f9
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-12 12:06:31 +0200

    Update lang.js

--- utils/lang.js
+++ utils/lang.js
@@ -1,5 +1,28 @@
-const store = {};
-module.exports = {
-  set: (id, lang) => store[id] = lang,
-  get: (id) => store[id] || "ar"
-};
+const fs = require("fs");
+const path = require("path");
+
+const file = path.join(__dirname, "../data/lang.json");
+
+function load() {
+  if (!fs.existsSync(file)) {
+    fs.writeFileSync(file, JSON.stringify({}, null, 2));
+  }
+  return JSON.parse(fs.readFileSync(file, "utf8"));
+}
+
+function save(data) {
+  fs.writeFileSync(file, JSON.stringify(data, null, 2));
+}
+
+function set(userId, lang) {
+  const data = load();
+  data[userId] = lang;
+  save(data);
+}
+
+function get(userId) {
+  const data = load();
+  return data[userId] || "ar"; // الافتراضي عربي
+}
+
+module.exports = { set, get };
