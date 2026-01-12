72598e5ae0bc732103a76725423025d3262d335c
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-12 09:48:27 +0200

    Create userStore.js

---
+++ utils/userStore.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+
+const file = path.join(__dirname, "../data/users.json");
+
+function load() {
+  if (!fs.existsSync(file)) return { users: {} };
+  return JSON.parse(fs.readFileSync(file, "utf8"));
+}
+
+function save(data) {
+  fs.writeFileSync(file, JSON.stringify(data, null, 2));
+}
+
+module.exports = {
+  add(username, expiresAt) {
+    const data = load();
+    data.users[username] = { expiresAt, active: true };
+    save(data);
+  },
+  disable(username) {
+    const data = load();
+    if (data.users[username]) {
+      data.users[username].active = false;
+      save(data);
+    }
+  },
+  list() {
+    return load().users;
+  }
+};
