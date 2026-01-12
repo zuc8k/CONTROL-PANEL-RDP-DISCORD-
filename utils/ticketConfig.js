728da0040306952e2dd1fd7b9459c2ffd8514966
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-12 11:12:43 +0200

    Update ticketConfig.js

--- utils/ticketConfig.js
+++ utils/ticketConfig.js
@@ -1,15 +1,46 @@
 const fs = require("fs");
 const path = require("path");
 
-const file = path.join(__dirname, "../data/ticketConfig.json");
+const dataDir = path.join(__dirname, "../data");
+const file = path.join(dataDir, "ticketConfig.json");
 
+// 🔒 تأكد إن فولدر data موجود
+function ensure() {
+  if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir, { recursive: true });
+  }
+
+  if (!fs.existsSync(file)) {
+    fs.writeFileSync(
+      file,
+      JSON.stringify(
+        {
+          category: null,
+          panelChannel: null,
+          logChannel: null,
+          panelImage: null,
+          insideImage: null
+        },
+        null,
+        2
+      )
+    );
+  }
+}
+
+// 📥 Load config
 function load() {
-  if (!fs.existsSync(file)) return {};
+  ensure();
   return JSON.parse(fs.readFileSync(file, "utf8"));
 }
 
+// 💾 Save config
 function save(data) {
+  ensure();
   fs.writeFileSync(file, JSON.stringify(data, null, 2));
 }
 
-module.exports = { load, save };
+module.exports = {
+  load,
+  save
+};
