2da54fe4844c7e0b9c029b4b4842f9f8319f09b6
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-12 09:35:35 +0200

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -1,12 +1,17 @@
 const { SlashCommandBuilder } = require("discord.js");
 const run = require("../utils/exec");
+const perm = require("../utils/permission");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("restart")
-    .setDescription("Restart the VPS"),
+    .setDescription("Restart VPS (Owner only)"),
 
   async execute(interaction) {
+    if (!perm.isOwner(interaction.user.id)) {
+      return interaction.reply({ content: "⛔ Owner only.", ephemeral: true });
+    }
+
     await interaction.reply("🔄 Restarting VPS...");
     await run("shutdown /r /t 5");
   }
