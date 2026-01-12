d4d2788368909dc483689ce35bf4be4d8998abcd
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-12 09:35:52 +0200

    Update shutdown.js

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,12 +1,17 @@
 const { SlashCommandBuilder } = require("discord.js");
 const run = require("../utils/exec");
+const perm = require("../utils/permission");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("shutdown")
-    .setDescription("Shutdown the VPS"),
+    .setDescription("Shutdown VPS (Owner only)"),
 
   async execute(interaction) {
+    if (!perm.isOwner(interaction.user.id)) {
+      return interaction.reply({ content: "⛔ Owner only.", ephemeral: true });
+    }
+
     await interaction.reply("⛔ Shutting down VPS...");
     await run("shutdown /s /t 5");
   }
